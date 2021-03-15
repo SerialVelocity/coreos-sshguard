f60680e0d7f35f68292c3fd6514f67603ae3d363
Ben Grabham <git@chillichef.com>
2021-03-15 11:05:05 +0000

    Filter journal on sshd
    Without this, it blacklists an IP instantly

--- start-sshguard.sh
+++ start-sshguard.sh
@@ -3,7 +3,7 @@
 set -eu
 
 BACKEND=${BACKEND:-/usr/lib/x86_64-linux-gnu/sshg-fw-ipset}
-LOGREADER=${LOGREADER:-LANG=C /bin/journalctl -afb -p info -n1 -D /var/log/journal -o cat SYSLOG_FACILITY=4 SYSLOG_FACILITY=10}
+LOGREADER=${LOGREADER:-LANG=C /bin/journalctl -afb -p info -n1 -D /var/log/journal -u sshd -o cat SYSLOG_FACILITY=4 SYSLOG_FACILITY=10}
 THRESHOLD=${THRESHOLD:-30}
 BLOCK_TIME=${BLOCK_TIME:-120}
 DETECTION_TIME=${DETECTION_TIME:-1800}
